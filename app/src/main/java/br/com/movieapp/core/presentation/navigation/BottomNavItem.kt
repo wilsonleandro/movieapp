9de75a3866c1a4dc98b7580cee70ca53140b9b6c
Wilson Leandro <wilson.bleandro@gmail.com>
2023-11-29 18:40:44 -0400

    Secao1

---
+++ app/src/main/java/br/com/movieapp/core/presentation/navigation/BottomNavItem.kt
@@ -0,0 +1,28 @@
+package br.com.movieapp.core.presentation.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Movie
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.ui.graphics.vector.ImageVector
+
+sealed class BottomNavItem(val title: String, val icon: ImageVector, val route: String) {
+    object MoviePopular: BottomNavItem(
+        title = "Filmes populares",
+        icon = Icons.Default.Movie,
+        route = "movie_polular_screen"
+    )
+
+    object MovieSearch: BottomNavItem(
+        title = "Pesquisar",
+        icon = Icons.Default.Search,
+        route = "movie_search_screen"
+    )
+
+    object MovieFavorite: BottomNavItem(
+        title = "Favoritos",
+        icon = Icons.Default.Favorite,
+        route = "movie_favorite_screen"
+    )
+
+}
